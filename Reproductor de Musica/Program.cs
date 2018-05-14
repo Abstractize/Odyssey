b74d0446ebbc439e271d9cbe7707cf904d7a4525
Steve <stevealv@hotmail.com>
2018-05-13 19:35:52 -0600

    Class songs created

--- Reproductor de Musica/Program.cs
+++ Reproductor de Musica/Program.cs
@@ -17,6 +17,7 @@ namespace Reproductor_de_Musica
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("Wakata");
         }
     }
 }
